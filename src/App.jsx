33c8dc4b88899a9caa5412292136245f4270c16d
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-28 20:50:28 +0100

    feat: add template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { MainContent } from 'widgets/mainContent';
+import { Links } from 'features/links';
+
+function App() {
+    return (
+        <>
+            <MainContent />
+            <Links />
+        </>
+    );
+}
+
+export default App;
